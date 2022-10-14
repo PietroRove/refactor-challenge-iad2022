f99ba122d24ac1aede90ca63eddab8101f086d04
Pietro Roversi <pietroroversi@libero.it>
2022-10-14 16:45:40 +0200

    [Commit2]: super refactor refuso

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,7 +1,6 @@
 import {FizzBuzz} from "./FizzBuzz";
 import {ComputeFizz} from "./ComputeFizz";
 
-const computeFizz = new ComputeFizz(3,"Fizz");
 
 let fizzBuzz = new FizzBuzz([new ComputeFizz(3,"Fizz"),new ComputeFizz(5,"Buzz")]);
 for (let inputNumber = 1; inputNumber <= 100; inputNumber++) {
